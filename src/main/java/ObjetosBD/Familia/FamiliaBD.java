5eee47b15d6cda350bdfb41c10521449cb3691d7
Ecoo <said7arteaga0mic@hotmail.com>
2026-08-29 20:03:48 -0600

    Persona COMPLETO

--- src/main/java/ObjetosBD/Familia/FamiliaBD.java
+++ src/main/java/ObjetosBD/Familia/FamiliaBD.java
@@ -2,6 +2,11 @@ package ObjetosBD.Familia;
 
 public class FamiliaBD {
 
+    public FamiliaBD(int id_familia, String apellidos) {
+        this.id_familia = id_familia;
+        this.apellidos = apellidos;
+    }
+
     private int id_familia;
     private String apellidos;
 
@@ -13,4 +18,8 @@ public class FamiliaBD {
     public String getApellidos(){return apellidos;}
     public void setApellidos(String apellidos){this.apellidos = apellidos;}
 
+    @Override
+    public String toString() {
+        return apellidos; // Muestra el apellido real en lugar de la memoria
+    }
 }
